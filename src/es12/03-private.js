81214c85a5d72fea50e883be2f583ddeaee7036c
danielecg <danielcastillo1408@gmail.com>
2022-11-25 18:28:22 -0300

    es12 y es13

---
+++ src/es12/03-private.js
@@ -0,0 +1,77 @@
+//declarando
+class User{};
+//instancia de una clase
+// const newUser = new User();
+
+class user{
+    //metodos
+    greeting() {
+        return "Hello";
+    }
+};
+
+const gndx = new user ();
+console.log(gndx.greeting());
+const bebeloper = new user();
+console.log(bebeloper.greeting());
+
+//constructor
+class user {
+    constructor(){
+        console.log("nuevo usuario");
+    }
+    greeting() {
+        return "Hello";
+    }
+}
+
+const david = new user();
+
+//this 
+class user {
+    constructor(name) {
+        this.name = name;
+    }
+    //metodos
+    speak(){
+        return "hello";
+    }
+    greeting(){
+        return `${this.speak()} ${this.name}`;
+    }
+}
+
+const ana = new user('Ana');
+console.log(ana.greeting());
+
+//getters y setters
+
+class user {
+    constructor(name,age) {
+        this.name = name;
+        this.age = age;
+
+    }
+    //metodos
+    #speak(){
+        return 'hello';
+    }
+    greeting(){
+        return `${this.speak()} ${this.name}`;
+    }
+ 
+    get #uAge(){
+        return this.age;
+    }
+
+    set #uAge(n) {
+        this.age = n;
+    }
+}
+
+const bebelover = new user ('david', 15);
+console.log(bebelover.uAge);
+console.log(bebelover.uAge = 20);
+
+
+
